98806b0960ee5e19a416a933e43fb3ab36f3ead7
azizahtas <azizahtas@gmail.com>
2019-11-20 12:41:59 +0530

    Initial Commit

---
+++ App/app.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template, request
+app = Flask(__name__,
+            static_url_path='', 
+            static_folder='web/static',
+            template_folder='web/templates')
+
+@app.route("/")
+def index():
+    return render_template('login.html',data={'title':"Zeftrosoft Base"})
+
+if __name__ == "__main__":
+    app.run(debug=True)
